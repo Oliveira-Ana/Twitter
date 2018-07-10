244ffd13fc341396ac0d28aa74f14bf908c99fbf
Ana Oliveira <ana_oli@outlook.com>
2018-07-10 15:45:22 -0300

    versão 0.0.2 e versão 0.0.6

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ submit.onclick = function (event){
   var input = document.querySelector('input');
   var container = document.getElementsByClassName('container')[0];
   var novaDiv = document.createElement('div');
-  novaDiv.textContent = input.value;
+  novaDiv.textContent = input.value + moment().format('lll');
   container.appendChild(novaDiv);
     event.preventDefault();
+}
+function desabilitar (){
+var limit = 140;
+var textLimit = document.querySelector('#textInput').value.length;
+var finish = limit - textLimit;
+if(finish>0){
+  document.getElementById('textLimit').innerHTML = finish;
+  document.getElementById('button').disabled = false;
+
+}else 
+{
+  document.getElementById('textLimit').innerHTML = finish;
+  document.getElementById('button').disabled = true;
+}
 }
