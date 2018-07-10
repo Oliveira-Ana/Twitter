7e6fc6599eaa0cb87af0c2d56428706e7c3bb7cd
Ana Oliveira <ana_oli@outlook.com>
2018-07-10 17:12:28 -0300

    ajustes 2.0

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var input = document.querySelector('input');
+
 function onclickDiv (){
   }
 var submit = document.querySelector('input[type=submit]');
@@ -6,6 +8,7 @@ submit.onclick = function (event){
   var container = document.getElementsByClassName('container')[0];
   var novaDiv = document.createElement('div');
   novaDiv.textContent = input.value + moment().format('lll');
+  novaDiv.className='styleColor';
   container.appendChild(novaDiv);
   textInput.value="";
   event.preventDefault();
@@ -24,3 +27,12 @@ if(finish>0){
   document.getElementById('button').disabled = true;
 }
 }
+function color(){
+var area = document.getElementById('textLimit');
+  if (input.value.length > 120 && input.value.length < 130){
+    area.style.color = "yellow";
+  } else if (input.value.length > 130 && input.value.length < 140){
+    area.style.color = "red";
+  }
+ }
+ setInterval(color, 1);
