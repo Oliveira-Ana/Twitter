ff2e46e37208f64c70885a9d6f5677504a9bec94
Ana Oliveira <ana_oli@outlook.com>
2018-07-10 16:51:35 -0300

    ajustes

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ submit.onclick = function (event){
   var novaDiv = document.createElement('div');
   novaDiv.textContent = input.value + moment().format('lll');
   container.appendChild(novaDiv);
-    event.preventDefault();
+  textInput.value="";
+  event.preventDefault();
 }
 function desabilitar (){
-var limit = 140;
-var textLimit = document.querySelector('#textInput').value.length;
-var finish = limit - textLimit;
+  var limit = 140;
+  var textLimit = document.querySelector('#textInput').value.length;
+  var finish = limit - textLimit;
 if(finish>0){
   document.getElementById('textLimit').innerHTML = finish;
   document.getElementById('button').disabled = false;
